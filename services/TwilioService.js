ca5239ac77a31058a36efd520856cc0d10a498e1
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-03-31 12:24:25 +0300

    disable sms service

--- services/TwilioService.js
+++ services/TwilioService.js
@@ -2,27 +2,30 @@ import axios from 'axios';
 import qs from 'qs';
 
 const URL = 'https://api.twilio.com/2010-04-01/Accounts/Messages.json';
-const AccountSID = 'AC2a8f29967db79c2037db492c87bd4e8f';
+const AccountSID = '<YourAccountSIDHere>';
+const AuthToken = '<YourAuthTokenHere>';
+const SenderNumber = '<YourSenderHere>';
 
 export default sendMessages = (to, message) => {
-	axios({
-		method: 'post',
-		url: 'https://api.twilio.com/2010-04-01/Accounts/AC2a8f29967db79c2037db492c87bd4e8f/Messages.json',
-		data: qs.stringify({
-			From: '+12037936369',
-			Body: message,
-			To: to,
-		}),
-		headers: {
-			'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-		},
-		auth: {
-			username: AccountSID,
-			password: '70734cf4dcfd76a0a6e827e1fea08c5e'
-		}
-	}).then((res) => {
-		console.log('Sent messages')
-	}).catch(error => {
-		console.log('Error sending msgs', error);
-	})
+	console.warn('SMS disabled, see TwilioService.js')
+	// axios({
+	// 	method: 'post',
+	// 	url: 'https://api.twilio.com/2010-04-01/Accounts/' + AccountSID + '/Messages.json',
+	// 	data: qs.stringify({
+	// 		From: SenderNumber,
+	// 		Body: message,
+	// 		To: to,
+	// 	}),
+	// 	headers: {
+	// 		'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+	// 	},
+	// 	auth: {
+	// 		username: AccountSID,
+	// 		password: AuthToken
+	// 	}
+	// }).then((res) => {
+	// 	console.log('Sent messages')
+	// }).catch(error => {
+	// 	console.log('Error sending msgs', error);
+	// })
 }
